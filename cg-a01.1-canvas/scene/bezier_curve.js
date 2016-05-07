6d36f190e91bc1d17dfe13938cbeaa5c69606e11
Jannik Portz <hello@jannikportz.de>
2016-05-07 17:48:00 +0200

    added doc for BezierCurve

--- cg-a01.1-canvas/scene/bezier_curve.js
+++ cg-a01.1-canvas/scene/bezier_curve.js
@@ -1,7 +1,25 @@
+/**
+ * Bezier Curve
+ * @authors Dennis Ritter, Jannik Portz, Nathalie Junker
+ *
+ * Module: BezierCurve
+ *
+ * A BezierCurve is described by its four control points.
+ * It provides draggers to change the control points i.e. the BezierCurve
+ * When a BezierCurve is selected it provides the ControlPolygon of the control points.
+ * The ControlPolygon does not offer any interaction.
+ */
+
 define(['Line', 'PointDragger', 'ControlPolygon'],
   function (Line, PointDragger, ControlPolygon) {
     "use strict";
 
+    /**
+     * Creates a new BezierCurve
+     * @param   controlPoints   Array of four [x,y] points which represent the initial control points
+     * @param   n               The number of linear segments to use for drawing the BezierCurve
+     * @param   lineStyle       The line style for the curve
+     */
     var BezierCurve = function (controlPoints, n, lineStyle) {
       if ( controlPoints.length !== 4 ) {
         throw new Error("You must specify exactly four controlPoints");
@@ -17,10 +35,14 @@ define(['Line', 'PointDragger', 'ControlPolygon'],
       this.generateLines();
     };
 
+    /**
+     * Updates the lines based on the current set of control points
+     */
     BezierCurve.prototype.generateLines = function () {
       this.points = [];
       this.lines = [];
 
+      // Generate n points and n-1 lines
       for ( var i = 0; i <= this.n; ++i ) {
         var t = i/this.n;
         this.points.push( this.generatePoint(t) );
@@ -31,6 +53,11 @@ define(['Line', 'PointDragger', 'ControlPolygon'],
       }
     };
 
+    /**
+     * Generates a point for the specified t value
+     * @param   t     The specified t value for which to generate the point
+     * @returns       [x,y] Point for the specified t
+     */
     BezierCurve.prototype.generatePoint = function (t) {
       var generateCoordinate = function ( factors ) {
         var coord = Math.pow(1-t, 3) * factors[0];
@@ -41,18 +68,33 @@ define(['Line', 'PointDragger', 'ControlPolygon'],
       };
 
       return [
+        // Only use the x values as factors
         generateCoordinate( this.controlPoints.map(function (p) {return p[0];}) ),
+
+        // Only use the y values as factors
         generateCoordinate( this.controlPoints.map(function (p) {return p[1];}) )
       ];
     };
 
+    /**
+     * Draws the BezierCurve into the specified context
+     * @param   context     The context in which to draw the BezierCurve
+     */
     BezierCurve.prototype.draw = function (context) {
+      // Draw each line
       for ( var i = 0; i < this.lines.length; ++i ) {
         this.lines[i].draw( context );
       }
     };
 
+    /**
+     * Checks whether the current mouse position hits the geometry
+     * @param   context   The drawing context that the geometry has been drawn into
+     * @param   mousePos  The current mouse position
+     * @returns           Whether the current mouse position hits the geometry
+     */
     BezierCurve.prototype.isHit = function (context, mousePos) {
+      // Check whether any of the lines is hit
       for ( var i = 0; i < this.lines.length; ++i ) {
         if ( this.lines[i].isHit(context, mousePos) ) {
           return true;
@@ -62,10 +104,15 @@ define(['Line', 'PointDragger', 'ControlPolygon'],
       return false;
     };
 
+    /**
+     * Creates the draggers for the BezierCurve
+     * @returns           Array of draggers
+     */
     BezierCurve.prototype.createDraggers = function () {
       var draggers = [];
       var _geo = this;
 
+      // Wrap into separate function to preserve the value of i
       var createControlPointDragger = function (i) {
         var getPosition = function () {
           return _geo.controlPoints[i];
