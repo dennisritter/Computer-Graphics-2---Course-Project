eda3baea71e7adaf9c7da2e96d564b65ae7a7af0
nathalie <BHT@Nathalies-MBP.home>
2016-05-19 20:36:44 +0200

    A.2.1.2 : Added BoxGeometry, RingGeometry and DodecahendronGeometry <3

--- uebung2/cg-a02.1-surfaces/controller/html_controller.js
+++ uebung2/cg-a02.1-surfaces/controller/html_controller.js
@@ -11,8 +11,8 @@
 
 
 /* requireJS module definition */
-define(["jquery", "BufferGeometry", "random", "band"],
-    (function($,BufferGeometry, Random, Band) {
+define(["jquery", "BufferGeometry", "random", "band", "three"],
+    (function($,BufferGeometry, Random, Band, THREE) {
         "use strict";
 
         /*
@@ -55,7 +55,7 @@ define(["jquery", "BufferGeometry", "random", "band"],
                     height : parseInt($("#height").attr("value"))
                 };
 
-
+                
                 var band = new Band(config);
                 var bufferGeometryBand = new BufferGeometry();
                 bufferGeometryBand.addAttribute("position", band.getPositions());
@@ -64,7 +64,35 @@ define(["jquery", "BufferGeometry", "random", "band"],
                 scene.addBufferGeometry(bufferGeometryBand);
             }));
 
+            $("#btnBox").click( (function(){
 
+                var box = new THREE.BoxGeometry(100,100,700);
+
+                var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+                var cube = new THREE.Mesh( box, material );
+
+                scene.scene.add(cube);
+            }));
+
+            $("#btnRing").click( (function(){
+
+                var ring = new THREE.RingGeometry(50,500,500);
+
+                var material = new THREE.MeshBasicMaterial( { color: 0xde0000 } );
+                var cube = new THREE.Mesh( ring, material );
+
+                scene.scene.add(cube);
+            }));
+
+            $("#btnDode").click( (function(){
+
+                var dode = new THREE.DodecahedronGeometry(300,0);
+
+                var material = new THREE.MeshBasicMaterial( { color: 0x0000de } );
+                var cube = new THREE.Mesh( dode, material );
+
+                scene.scene.add(cube);
+            }));
         };
 
         // return the constructor function
