fd0b1f4f44d119ebd6ade6acd55e18ee721ced28
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-06-18 14:19:20 +0200

    added right arm

--- uebung2/cg-a02.1-surfaces/models/robot.js
+++ uebung2/cg-a02.1-surfaces/models/robot.js
@@ -5,20 +5,20 @@ define(["three"],
 
         "use strict";
 
-        var Robot = function(){
+        var Robot = function() {
 
             var minUnit = 30;
             var segments = 32;
 
             /* Define the size for every element */
-            var torsoSize   = [minUnit * 3, minUnit * 10];  // radius, length of the cylinder
-            var neckSize    = [minUnit, minUnit];           // radius, length of the cylinder
-            var headSize    = minUnit * 5;                  // diameter of the sphere, if head was a box the size would be minUnit * 4
-            var jointSize   = minUnit;                      // radius of the sphere
-            var armSize     = [minUnit, minUnit * 6];       // radius, length of the cylinder, upper arm and forearm are eqal
-            var handSize    = minUnit;                      // radius of the sphere
-            var legSize     = [armSize[0],armSize[1]];      // legs are as long and big as the robot's arms
-            var footSize    = handSize;                     // radius, feet are as bis as hands
+            var torsoSize = [minUnit * 3, minUnit * 10];  // radius, length of the cylinder
+            var neckSize = [minUnit, minUnit];           // radius, length of the cylinder
+            var headSize = minUnit * 5;                  // diameter of the sphere, if head was a box the size would be minUnit * 4
+            var jointSize = minUnit;                      // radius of the sphere
+            var armSize = [minUnit, minUnit * 6];       // radius, length of the cylinder, upper arm and forearm are eqal
+            var handSize = minUnit;                      // radius of the sphere
+            var legSize = [armSize[0], armSize[1]];      // legs are as long and big as the robot's arms
+            var footSize = handSize;                     // radius, feet are as bis as hands
 
             this.root = new THREE.Object3D();
 
@@ -26,8 +26,8 @@ define(["three"],
 
             this.torso = new THREE.Object3D();
             this.torso.name = "torso";
-            this.torsoSkin = new THREE.Mesh ( new THREE.CylinderGeometry( torsoSize[0], torsoSize[0],
-                torsoSize[1], segments ), new THREE.MeshNormalMaterial());
+            this.torsoSkin = new THREE.Mesh(new THREE.CylinderGeometry(torsoSize[0], torsoSize[0],
+                torsoSize[1], segments), new THREE.MeshNormalMaterial());
             this.torso.add(this.torsoSkin);
             this.root.add(this.torso);
 
@@ -37,11 +37,11 @@ define(["three"],
             this.neck = new THREE.Object3D();
             this.neck.name = "neck";
             //move the skeleton from the center up to the upperside of the torso's skin. (A)
-            this.neck.translateY(torsoSize[1]/2);
-            this.neckSkin = new THREE.Mesh ( new THREE.SphereGeometry( jointSize, segments, segments ),
+            this.neck.translateY(torsoSize[1] / 2);
+            this.neckSkin = new THREE.Mesh(new THREE.SphereGeometry(jointSize, segments, segments),
                 new THREE.MeshNormalMaterial());
             //move the skin from the middle of the skeleton up to the top of the torso's skin (B)
-            this.neckSkin.translateY(neckSize[1]/2);
+            this.neckSkin.translateY(neckSize[1] / 2);
             this.neck.add(this.neckSkin);
             this.torso.add(this.neck);
 
@@ -52,83 +52,131 @@ define(["three"],
             this.head.name = "head";
 
             //move the skeleton from the center of the neck up to the upperside of the neck's skin:
-            this.head.translateY(neckSize[1]/2);
+            this.head.translateY(neckSize[1] / 2);
 
             // you can't see a sphere rotating, therefore we need a box as head:
             // this.headSkin = new THREE.Mesh ( new THREE.BoxGeometry(headSize, headSize, headSize),
             //     new THREE.MeshNormalMaterial());
 
-            this.headSkin = new THREE.Mesh ( new THREE.SphereGeometry(headSize/2, segments, segments),
+            this.headSkin = new THREE.Mesh(new THREE.SphereGeometry(headSize / 2, segments, segments),
                 new THREE.MeshNormalMaterial());
 
             //move the skin from the middle of this skeleton up to the top of the necks's skin:
-            this.headSkin.translateY(headSize/2);
+            this.headSkin.translateY(headSize / 2);
             this.head.add(this.headSkin);
             this.neck.add(this.head);
 
+            /* = = = = = = = = = = = = = = = = = = = = = = = /
+            /* * * * * * * * * * LEFT ARM * * * * * * * * * */
+            /* = = = = = = = = = = = = = = = = = = = = = = = /
 
             /* JOINT LEFT UPPER ARM = jointLua */
-
             this.jointLua = new THREE.Object3D();
             this.jointLua.name = "jointLUA";
 
             //move the skeleton from the center to the left upper corner of the torso's skin:
-            this.jointLua.translateY( torsoSize[1]/2 );
-            this.jointLua.translateX( torsoSize[0] );
-            this.jointLua.translateY( -jointSize );
-            this.jointLuaSkin = new THREE.Mesh( new THREE.SphereGeometry(jointSize, segments, segments), new THREE.MeshNormalMaterial());
+            this.jointLua.translateY(torsoSize[1] / 2);
+            this.jointLua.translateX(torsoSize[0]);
+            this.jointLua.translateY(-jointSize);
+            this.jointLuaSkin = new THREE.Mesh(new THREE.SphereGeometry(jointSize, segments, segments), new THREE.MeshNormalMaterial());
             this.jointLua.add(this.jointLuaSkin);
             this.torso.add(this.jointLua);
 
 
             /* LEFT UPPER ARM = LUA */
-
             this.lua = new THREE.Object3D();
             this.lua.name = "lua";
-            this.luaSkin = new THREE.Mesh( new THREE.CylinderGeometry(armSize[0], armSize[0], armSize[1], segments),
+            this.luaSkin = new THREE.Mesh(new THREE.CylinderGeometry(armSize[0], armSize[0], armSize[1], segments),
                 new THREE.MeshNormalMaterial());
 
             //move the skin from the center of the joint ( = skeleton-center ) to the right position
-            this.luaSkin.rotateZ(Math.PI/2);
-            this.luaSkin.translateY(-armSize[1]/2);
+            this.luaSkin.rotateZ(Math.PI / 2);
+            this.luaSkin.translateY(-armSize[1] / 2);
 
             this.lua.add(this.luaSkin);
             this.jointLua.add(this.lua);
 
 
             /* JOINT LEFT FOREARM = JointLfa */
-
             this.jointLfa = new THREE.Object3D();
             this.jointLfa.name = "jointLFA";
 
             //move the skeleton from the center to the end of the left upper arm:
-            this.jointLfa.translateX( armSize[1] );
-            this.jointLfaSkin = new THREE.Mesh( new THREE.SphereGeometry(jointSize, segments, segments), new THREE.MeshNormalMaterial());
+            this.jointLfa.translateX(armSize[1]);
+            this.jointLfaSkin = new THREE.Mesh(new THREE.SphereGeometry(jointSize, segments, segments), new THREE.MeshNormalMaterial());
             this.jointLfa.add(this.jointLfaSkin);
             this.lua.add(this.jointLfa);
-
 
 
             /* LEFT FOREARM = LFA */
             this.lfa = new THREE.Object3D();
             this.lfa.name = "lfa";
-            this.lfaSkin = new THREE.Mesh( new THREE.CylinderGeometry(armSize[0], armSize[0], armSize[1], segments),
+            this.lfaSkin = new THREE.Mesh(new THREE.CylinderGeometry(armSize[0], armSize[0], armSize[1], segments),
                 new THREE.MeshNormalMaterial());
             //move the skin from the center of the joint ( = skeleton-center ) to the right position
-            this.lfaSkin.rotateZ(Math.PI/2);
-            this.lfaSkin.translateY(-armSize[1]/2);
+            this.lfaSkin.rotateZ(Math.PI / 2);
+            this.lfaSkin.translateY(-armSize[1] / 2);
             this.lfa.add(this.lfaSkin);
             this.jointLfa.add(this.lfa);
 
+            /* = = = = = = = = = = = = = = = = = = = = = = = /
+            /* * * * * * * * * * RIGHT ARM * * * * * * * * * */
+            /* = = = = = = = = = = = = = = = = = = = = = = = /
+
+            /* JOINT RIGHT UPPER ARM = jointRua */
+            this.jointRua = new THREE.Object3D();
+            this.jointRua.name = "jointRUA";
+
+            //move the skeleton from the center to the right upper corner of the torso's skin:
+            this.jointRua.translateY(torsoSize[1] / 2);
+            this.jointRua.translateX(-torsoSize[0]);
+            this.jointRua.translateY(-jointSize);
+            this.jointRuaSkin = new THREE.Mesh(new THREE.SphereGeometry(jointSize, segments, segments), new THREE.MeshNormalMaterial());
+            this.jointRua.add(this.jointRuaSkin);
+            this.torso.add(this.jointRua);
 
 
-            
+            /* RIGHT UPPER ARM = RUA */
+            this.rua = new THREE.Object3D();
+            this.rua.name = "rua";
+            this.ruaSkin = new THREE.Mesh(new THREE.CylinderGeometry(armSize[0], armSize[0], armSize[1], segments),
+                new THREE.MeshNormalMaterial());
+
+            //move the skin from the center of the joint ( = skeleton-center ) to the right position
+            this.ruaSkin.rotateZ(-Math.PI / 2);
+            this.ruaSkin.translateY(-armSize[1] / 2);
+
+            this.rua.add(this.ruaSkin);
+            this.jointRua.add(this.rua);
+
+            /* JOINT RIGHT FOREARM = JointRfa */
+            this.jointRfa = new THREE.Object3D();
+            this.jointRfa.name = "jointRFA";
+
+            //move the skeleton from the center to the end of the right upper arm:
+            this.jointRfa.translateX(-armSize[1]);
+            this.jointRfaSkin = new THREE.Mesh(new THREE.SphereGeometry(jointSize, segments, segments), new THREE.MeshNormalMaterial());
+            this.jointRfa.add(this.jointRfaSkin);
+            this.rua.add(this.jointRfa);
+
+
+            /* RIGHT FOREARM = RFA */
+            this.rfa = new THREE.Object3D();
+            this.rfa.name = "rfa";
+            this.rfaSkin = new THREE.Mesh(new THREE.CylinderGeometry(armSize[0], armSize[0], armSize[1], segments),
+                new THREE.MeshNormalMaterial());
+            //move the skin from the center of the joint ( = skeleton-center ) to the right position
+            this.rfaSkin.rotateZ(-Math.PI / 2);
+            this.rfaSkin.translateY(-armSize[1] / 2);
+            this.rfa.add(this.rfaSkin);
+            this.jointRfa.add(this.rfa);
+
 
         };
 
-        Robot.prototype.getMesh = function () {
-          return this.root;  
+        Robot.prototype.getMesh = function() {
+            return this.root;
         };
-        
+
         return Robot
     }));
