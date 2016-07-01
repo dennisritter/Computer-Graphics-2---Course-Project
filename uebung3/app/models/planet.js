c43942a7e90f26713486552f8351a0867ac503c1
Dennis Ritter <dennis.r.ritter@gmail.com>
2016-07-01 20:55:16 +0200

    started implementing planet.js

--- uebung3/app/models/planet.js
+++ uebung3/app/models/planet.js
@@ -13,21 +13,37 @@ define(["three", "shaders"],
             
             var scope = this;
 
-            // implement ShaderMaterial using the code from
-            // the lecture
+            var material = new THREE.ShaderMaterial( {
+                uniforms: THREE.UniformsUtils.merge( [
+                    THREE.UniformsLib['lights'],
+                    {
+                        diffuseMaterial: { type: 'c', value: new THREE.Color(1,0,0) },
+                        specularMaterial: { type: 'c', value: new THREE.Color(0.7,0.7,0.7) },
+                        ambientMaterial: { type: 'c', value: new THREE.Color(0.8,0.2,0.2) },
+                        shininessMaterial: { type: 'f', value: 16.0 },
+                        texEarthMonth04: { type: 't', value: ""}
+                        //add other textures here
+                    }
+                ]),
+                vertexShader: Shaders.getVertexShader("planet"),
+                fragmentShader: Shaders.getFragmentShader("planet"),
+                lights: true
+            } );
             
             // hint:
             // texture can be assigned only when it is loaded completely
             // and then can be set like any other uniform variable
             // material.uniforms.<uniform-var-name>.value   = <uniform-value>;
+            var texLoader = new THREE.TextureLoader();
+            texLoader.load("textures/earth_month04.jpg", function(tex){
+                material.uniforms.texEarthMonth04.value = tex;
+            });
+
             
             scope.mesh = new THREE.Mesh( new THREE.SphereGeometry(400, 100,100), material );
             scope.mesh.name = "planet";
 
             scope.root.add(scope.mesh);
-
-
-
 
             this.getMesh = function() {
                 return this.root;
